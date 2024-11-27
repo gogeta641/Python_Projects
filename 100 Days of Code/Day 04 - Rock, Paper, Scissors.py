c9aa34ce94f8cdcfe94f41387c51009166013eec
gogeta641 <karthik.gogeta@gmail.com>
2024-11-27 11:13:26 +0530

    Day 4 Project Update

---
+++ 100 Days of Code/Day 04 - Rock, Paper, Scissors.py
@@ -0,0 +1,61 @@
+import random
+
+rock = '''
+Rock
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+Paper
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+Scissors
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+player = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors "))
+game = [rock, paper, scissors]
+print(game[player])
+
+computer = random.randint(0,2)
+print("Computer chose: ")
+print(game[computer])
+
+if player == 0:
+    if computer == 0:
+        print("It's a draw!")
+    elif computer == 1:
+        print("You lose!")
+    else:
+        print("You won!")
+elif player == 1:
+    if computer == 0:
+        print("You won!")
+    elif computer == 1:
+        print("It's a draw!")
+    else:
+        print("You lose!")
+elif player == 2:
+    if computer == 0:
+        print("You lose!")
+    elif computer == 1:
+        print("You won!")
+    else:
+        print("It's a draw!")
